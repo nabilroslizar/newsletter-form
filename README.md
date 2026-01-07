f3760d1f63e249366361db0b5cfdeaf8e9aefcc5
Nabil <nabil.roslizar@gmail.com>
2026-01-07 16:59:26 +0100

    js validation toggling

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ NEWSLETTER SIGNUP FORM
 - Step 1 - Build the HTML
     - sign up form view
     - sucess message
+- Step 2 - Add CSS foundation (so layout wont fight you)
+    - baseline color, font, page height, toggling
+- Step 3 : JS + Validation
+    - when user submits:
+    - show error if empty
+    - show error if invalid email
+    - otherwise switch to success screen + show their email
+    - dismiss button returns to form
+-Step 4 : Real Layout + Responsiveness
---
+++ script.js
@@ -0,0 +1,60 @@
+const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("emailError");
+
+const signupCard = document.getElementById("signupCard");
+const successCard = document.getElementById("successCard");
+const successEmail = document.getElementById("successEmail");
+const dismissBtn = document.getElementById("dismissBtn");
+
+console.log({ form, emailInput, emailError, signupCard, successCard, successEmail, dismissBtn });
+
+function showError (message ){
+    emailError.textContent= message;
+    emailInput.classList.add("input--error");
+}
+
+function clearError (){
+    emailError.textContent= "";
+    emailInput.classList.remove("input--error");
+}
+
+function isValidEmail(value){
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+} 
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    const email = emailInput.value.trim();
+  
+    if (!email) {
+      showError("Email is required");
+      return;
+    }
+  
+    if (!isValidEmail(email)) {
+      showError("Valid email required");
+      return;
+    }
+  
+    clearError();
+    successEmail.textContent = email;
+
+    signupCard.classList.add("hidden");
+    successCard.classList.remove("hidden");
+  });
+
+  dismissBtn.addEventListener("click", ()=> {
+    successCard.classList.add("hidden");
+    signupCard.classList.remove("hidden");
+
+    emailInput.value = "";
+    clearError();
+    emailInput.focus();
+  })
+
+emailInput.addEventListener("input", ()=> {
+    clearError();
+})
